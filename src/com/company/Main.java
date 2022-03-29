89efb0fb4478dc9cac1ff141d9da08778a17dae5
Alok KumarS Singh <singhalokkumar427@gmail.com>
2022-03-29 23:20:36 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String s1 = "ravit";
+        String s2 = "ravit";
+        String s3 = "ramesh";
+        System.out.println(s1.compareTo(s2));
+        System.out.println(s1.compareTo(s3));
+        System.out.println(s3.compareTo(s1));
+	// write your code here
+    }
+}
